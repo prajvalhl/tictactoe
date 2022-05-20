e1df56376035bdb74a9c294a538ea9008e0fbdbd
Prajval <prajvalhl@gmail.com>
2022-05-20 18:24:43 +0530

    modified by adding third party libraries

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,121 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Hi Tic</p>
+  <button @click="checkIsWinner">click</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Icon from "./components/Icon.vue";
+import Swal from "sweetalert2/dist/sweetalert2";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Icon },
+  data() {
+    return {
+      winMessage: "",
+      isCross: true,
+      itemArray: [],
+      // itemArr: new Array(9).fill("empty"),
+    };
+  },
+  watch: {
+    winMessage: function (msg) {
+      if (msg) {
+        this.showDialog();
+      }
+    },
+  },
+  methods: {
+    showDialog() {
+      Swal.fire({
+        icon: "info",
+        title: "Game Over",
+        text: `${this.winMessage}`,
+      });
+    },
+    handleClick(itemNum) {
+      if (this.winMessage) {
+        return this.showDialog();
+      }
+
+      if (this.itemArray[itemNum]) {
+        this.itemArray[itemNum] = this.isCross ? "cross" : "circle";
+        this.isCross = !this.isCross;
+      } else {
+        return Swal.fire("Already filled");
+      }
+
+      this.checkIsWinner();
+    },
+    checkIsWinner() {
+      if (
+        this.itemArray[0] === this.itemArray[1] &&
+        this.itemArray[0] === this.itemArray[2] &&
+        this.itemArray[0]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[3] &&
+        this.itemArray[3] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[5]
+      ) {
+        this.winMessage = `${this.itemArray[3]} won`;
+      } else if (
+        this.itemArray[6] &&
+        this.itemArray[6] === this.itemArray[7] &&
+        this.itemArray[7] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[6]} won`;
+      } else if (
+        this.itemArray[0] &&
+        this.itemArray[0] === this.itemArray[3] &&
+        this.itemArray[3] === this.itemArray[6]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[1] &&
+        this.itemArray[1] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[7]
+      ) {
+        this.winMessage = `${this.itemArray[1]} won`;
+      } else if (
+        this.itemArray[2] &&
+        this.itemArray[2] === this.itemArray[5] &&
+        this.itemArray[5] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[2]} won`;
+      } else if (
+        this.itemArray[0] &&
+        this.itemArray[0] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[8]
+      ) {
+        this.winMessage = `${this.itemArray[0]} won`;
+      } else if (
+        this.itemArray[2] &&
+        this.itemArray[2] === this.itemArray[4] &&
+        this.itemArray[4] === this.itemArray[6]
+      ) {
+        this.winMessage = `${this.itemArray[2]} won`;
+      }
+    },
+    reloadGame() {
+      (this.winMsg = ""), (this.isCross = true), (this.itemArr = []);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+main {
+  height: 100vh;
+}
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 5px;
+}
+
+.box {
+  height: 150px;
 }
 </style>
